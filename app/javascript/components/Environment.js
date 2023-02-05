e3f935df1de8ee3540c3084f8fe83f353acfb419
Michael <mineylan@gmail.com>
2023-02-05 11:29:36 -0800

    update Railway url

--- app/javascript/components/Environment.js
+++ app/javascript/components/Environment.js
@@ -3,7 +3,7 @@ const development = {
 }
 
 const production = {
-  url: "where-is-waldo-production.up.railway.app"
+  url: "https://where-is-waldo-production.up.railway.app"
 }
 
 export const environment = process.env.NODE_ENV === 'development' ? development : production;
